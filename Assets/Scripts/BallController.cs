8331b7e90a038b1bbf3d327d9519779b5c5cea78
Bohdan Zaichenko <ichim.akih1r0@gmail.com>
2020-04-07 11:24:18 +0200

    Swipe

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,6 +7,9 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb2d;
     private bool StartPositionIsCompleted = false;
+    private bool tap, swipeUp, doubleTap;
+    private Vector2 swipeDelta, startTouch;
+    public float deadZone = 100f;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -18,9 +21,57 @@ public class BallController : MonoBehaviour
     {
         if (rb2d.transform.position.y < -3.0f && !StartPositionIsCompleted)
         {
-            Debug.Log("Suka");
+          
             rb2d.velocity = Vector2.zero;
             StartPositionIsCompleted = true;
-        } 
+        }
+
+        tap = swipeUp = doubleTap = false;
+        //Check for input
+        if (Input.touches.Length != 0)
+        {
+            if (Input.touches[0].phase==TouchPhase.Began)
+            {
+                tap = true;
+                startTouch = Input.mousePosition;
+
+            }
+            else if (Input.touches[0].phase==TouchPhase.Ended|| Input.touches[0].phase==TouchPhase.Canceled)
+            {
+                startTouch = swipeDelta = Vector2.zero;
+            }
+        }
+
+
+        //Calculate swipes
+        swipeDelta = Vector2.zero;
+        if (startTouch!=Vector2.zero)
+        {
+            if (Input.touches.Length!=0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch; //swipe exist
+            }
+            
+        }
+
+        if (swipeDelta.magnitude>deadZone)
+        {
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(y)>Mathf.Abs(x))
+            {
+                if (y>0)
+                {
+                    swipeUp = true;
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+        else if (Input.touches.Length != 0)
+        { 
+            tap = true;
+            transform.Translate(0, 4, 0);
+        }
+
     }
 }
